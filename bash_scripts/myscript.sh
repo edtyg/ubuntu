a6f2b365b811eb6dd687c6758e793aba63958087
Edgar Tan <edgartan93@hotmail.com>
2023-03-15 12:19:09 +0800

    add

--- bash_scripts/myscript.sh
+++ bash_scripts/myscript.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-ls
-pwd
+myname="edt"
+myage=26
+files=$(ls)
+directory=$(pwd)
 
 echo "hello world"
+echo "hi my name is $myname and my age is $myage"
+echo 'hi my name is $myname and my age is $myage'
+echo $files
+echo $directory
